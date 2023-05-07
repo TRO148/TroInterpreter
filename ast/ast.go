2bb8df5905d0a6fa5e4d3c1e3d54829cbcf5f851
trotro <1481895142@qq.com>
2023-05-07 20:38:48 +0800

    文件调整

--- ast/ast.go
+++ ast/ast.go
@@ -1,21 +1,61 @@
 package ast
 
-/*
-接口，所有的节点都要实现这个接口
-*/
+import (
+	"TroInterpreter/token"
+	"bytes"
+)
 
+// ast节点都应实现Node接口
 type Node interface {
 	TokenLiteral() string
-
 	String() string
 }
 
+// 语句
 type Statement interface {
 	Node
 	statementNode()
 }
 
+// 表达式
 type Expression interface {
 	Node
 	expressionNode()
+}
+
+// 程序
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 { // 语句不为空
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+func (p *Program) String() string {
+	var out bytes.Buffer // 字节缓冲区
+
+	//读取
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
+// 标识符
+type Identifier struct {
+	Token token.Token // token.IDENT
+	Value string      // 标识符的值
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
+func (i *Identifier) String() string {
+	return i.Value
 }
