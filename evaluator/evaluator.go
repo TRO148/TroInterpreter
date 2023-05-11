0b9f6a624d53998f2c94eae47a7f4b64cd10ab7f
trotro <1481895142@qq.com>
2023-05-12 00:20:31 +0800

    前缀表达式

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -14,7 +14,6 @@ var (
 // 求值
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
-
 	//分析程序
 	case *ast.Program:
 		return evalStatements(node.Statements)
@@ -30,6 +29,11 @@ func Eval(node ast.Node) object.Object {
 		//分析布尔值
 	case *ast.Boolean:
 		return bool2BoolObject(node.Value)
+
+		//分析前缀表达式
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
 	return nil
@@ -52,4 +56,42 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
+}
+
+// 求值前缀表达式
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+// 布尔值
+func evalBangOperatorExpression(right object.Object) object.Object {
+	//操作数在为负或为NULL可以明确表示出来，就用TRUE
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+// 负数
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	//先检查操作数是不是整数，不是就返回NULL
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
 }
